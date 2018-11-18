53f54703c55be75451e6f8bd76fdc711945c1b75
skuethe <crazycapivara@gmail.com>
2018-11-18 18:15:16 +0100

    Add get-started examples

---
+++ inst/examples/get-started.R
@@ -0,0 +1,14 @@
+library(h3forr)
+
+lat <- 37.3615593
+lng <- -122.0553238
+res <- 7
+
+# Convert a lat/lng point to a hexagon index at resolution 7
+h3_index <- geo_to_h3(lat, lng, res)
+
+# Get the center of the hexagon
+hex_center_coordinates <- h3_to_geo(h3_index)
+
+# Get the vertices of the hexagon
+h3_to_geo_boundary(h3_index)
