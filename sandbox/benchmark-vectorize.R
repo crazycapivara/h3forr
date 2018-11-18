8f6f041f51f99e6cb79c146e5d7737623ad3dc1b
skuethe <crazycapivara@gmail.com>
2018-11-18 23:10:30 +0100

    Update benchmark

--- sandbox/benchmark-vectorize.R
+++ sandbox/benchmark-vectorize.R
@@ -6,5 +6,6 @@ h3_index <- geo_to_h3_map(us_cities[1:50, ], "lat", "long")
 
 microbenchmark(
   lapply(h3_index, h3_to_geo_boundary),
+  purrr::map(h3_index, h3_to_geo_boundary),
   h3_to_geo_boundary(h3_index)
 )
