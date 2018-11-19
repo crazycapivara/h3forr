2cfc9c6421f64d8002ad34c5125c802d4517a226
skuethe <crazycapivara@gmail.com>
2018-11-19 22:44:07 +0100

    Refactor js-vectorize function

--- R/settings.R
+++ R/settings.R
@@ -6,15 +6,15 @@ use_h3js <- function() {
     pkg_env$ctx$source()
 }
 
+.onLoad <- function(libname, pkgname) {
+  use_h3js()
+}
+
 # TODO: rename to 'h3js'
 h3js_call <- function(func_name, ...) {
   pkg_env$ctx$call(func_name, ...)
 }
 
-h3js_map <- function(func_name, data) {
-  pkg_env$ctx$call("vectorize", func_name, data)
-}
-
-.onLoad <- function(libname, pkgname) {
-  use_h3js()
+h3js_map <- function(func_name, data, ...) {
+  pkg_env$ctx$call("vectorize", func_name, data, list(...))
 }
