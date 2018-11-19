6d7270b1467b82828a3c4614f0f0bb0f731bbac1
skuethe <crazycapivara@gmail.com>
2018-11-19 22:51:11 +0100

    Update examples

--- inst/examples/h3-vectorized.R
+++ inst/examples/h3-vectorized.R
@@ -1,5 +1,7 @@
 library(geojsonio)
 library(h3forr)
 
-h3_index <- geo_to_h3_map(us_cities[1:50, ], "lat", "long")
+h3_index <- geo_to_h3(us_cities[1:50, c("lat", "long")])
+h3_to_geo(h3_index)
+h3_to_geo_boundary(h3_index[1])
 h3_to_geo_boundary(h3_index)
