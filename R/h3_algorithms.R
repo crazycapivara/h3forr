2cd4256fcd915615bc670e6d1949a863759f08b9
skuethe <crazycapivara@gmail.com>
2018-11-20 22:41:54 +0100

    Add 'set_to_multi_polygon' function

--- R/h3_algorithms.R
+++ R/h3_algorithms.R
@@ -57,3 +57,21 @@ hex_ring <- function(h3_index, ring_size = 1) {
 polyfill <- function(coords, res = 7, is_geojson = TRUE) {
   h3js("polyfill", coords, res, is_geojson)
 }
+
+#' Get the outlines of a set of H3 hexagons.
+#'
+#' Returns GeoJSON MultiPolygon format
+#' (an array of polygons, each with an array of loops, each an array of coordinates).
+#' Coordinates are returned as [lat, lng] pairs unless GeoJSON is requested.
+#'
+#' @inheritParams h3_to_geo_boundary
+#'
+#' @example inst/examples/api-examples/h3-set-to-multipolygon.R
+#'
+#' @export
+h3_set_to_multi_polygon <- function(h3_index, format_as_geojson = TRUE) {
+  res <- h3js("h3SetToMultiPolygon", h3_index, format_as_geojson) %>%
+    as.vector() %>% matrix(ncol = 2)
+  if (format_as_geojson) res %<>% h3forr_class("lng_lat_closed")
+  res
+}
