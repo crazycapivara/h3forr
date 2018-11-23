e53a94297b5cf5d9ae8e6f0c9f43f37e78ac7f0c
skuethe <crazycapivara@gmail.com>
2018-11-23 14:19:57 +0100

    Add function to get sample h3 edge index

--- R/custom_utils.R
+++ R/custom_utils.R
@@ -18,3 +18,11 @@ get_sample_h3_index <- function() {
 get_sample_h3_indexes <- function() {
   get_sample_h3_index() %>% k_ring()
 }
+
+#' Get sample h3 edge index
+#'
+#' @export
+get_sample_h3_edge_index <- function() {
+  indexes <- get_sample_h3_indexes()
+  get_h3_unidirectional_edge(indexes[1], indexes[2])
+}
