f03603d06ebaaed47cbfba9710a0ff8981cc769e
skuethe <crazycapivara@gmail.com>
2018-11-24 21:07:23 +0100

    Add 'manhattan' dataset

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Persons in manhattan
+#'
+#' @format a tibble with 13987 rows and 3 variables:
+#' \describe{
+#'   \item{lng}{longitude}
+#'   \item{lat}{latitude}
+#'   \item{sex}{1: male, 2: female}
+#' }
+#'
+#' @source \url{https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/scatterplot/manhattan.json}
+"manhattan"
