e1c07bdeb18659857b076efb436cdce925930b8a
skuethe <crazycapivara@gmail.com>
2018-11-26 21:42:16 +0100

    Add polyfill tests

--- tests/testthat/test_polyfill.R
+++ tests/testthat/test_polyfill.R
@@ -1,14 +1,16 @@
 context("polyfill")
 
-test_that("matrix", {
+test_that("matrix and data frame", {
   # Prepare
   polygon <- greater_manchester
   resolution <- 5
 
   # Act
   h3_indexes <- polyfill(polygon, resolution)
+  h3_indexes2 <- polyfill(as.data.frame(polygon), resolution)
 
   # Assert
   expect_length(h3_indexes, 4)
   expect_is(h3_indexes, "character")
+  expect_equal(h3_indexes, h3_indexes2)
 })
