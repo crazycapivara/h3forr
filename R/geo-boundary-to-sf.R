90119c62a7b9d297ca1cdc59b1ffee1bd7ad365a
skuethe <crazycapivara@gmail.com>
2018-11-27 22:33:29 +0100

    Add tests for 'geo_boundary_to_sf'

--- R/geo-boundary-to-sf.R
+++ R/geo-boundary-to-sf.R
@@ -1,22 +1,23 @@
-# TODO: obsolete, remove it!
+## TODO: obsolete, remove it!
+#h3_as_sf <- function(data) {
+#  features <- lapply(1:dim(data)[3], function(i) {
+#    list(data[, , i]) %>%
+#      sf::st_polygon()
+#  })
+#  sf::st_sfc(features, crs = 4326) %>%
+#    sf::st_sf()
+#}
 
-h3_as_sf <- function(data) {
-  features <- lapply(1:dim(data)[3], function(i) {
-    list(data[, , i]) %>%
-      sf::st_polygon()
-  })
-  sf::st_sfc(features, crs = 4326) %>%
-    sf::st_sf()
-}
-
-## NEW
 features_to_sf <- function(features) {
   sf::st_sfc(features, crs = 4326) %>% sf::st_sf()
 }
 
-#' Parse geo boundary array to sf object
+#' Parse geo boundaries to object of class \code{sf}
 #'
-#' @param obj geo boundary array
+#' @param obj geo boundary matrix or list of matrices with [lng, lat] pairs
+#' as returned from \link{h3_to_geo_boundary} or \link{h3_set_to_multi_polygon}
+#'
+#' @return object of class \code{sf} (geometry type: \code{POLYGON})
 #'
 #' @name geo_boundary_to_sf
 #' @export
@@ -30,7 +31,7 @@ geo_boundary_to_sf <- function(obj) {
 
 #' @name geo_boundary_to_sf
 #' @export
-geo_boundary_to_sf.default <- function(obj) {
+geo_boundary_to_sf.matrix <- function(obj) {
   sf::st_polygon(list(obj)) %>% features_to_sf()
 }
 
