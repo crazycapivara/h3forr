25948df732ff98ceafada93fa87d22c3627837cd
skuethe <crazycapivara@gmail.com>
2018-11-27 21:29:10 +0100

    Refactor

--- R/h3_algorithms.R
+++ R/h3_algorithms.R
@@ -48,49 +48,6 @@ hex_ring <- function(h3_index, ring_size = 1) {
   h3js_map("hexRing", h3_index, ring_size)
 }
 
-#' Get all hexagons with centers contained in a given polygon
-#'
-#' @param coords numeric vector, matrix or data.frame with [lat, lng]
-#' or [lng, lat] pairs if \code{is_geojson = TRUE};
-#' object of class \code{sf} of type \code{POLYGON} or \code{MULTIPOLYGON}
-#' @param res resolution; number between 0 and 15
-#' @param is_geojson expect [lng, lat] pairs instead of [lat, lng]?
-#'
-#' @example inst/examples/api-reference/polyfill.R
-#'
-#' @name polyfill
-#' @export
-polyfill <- function(coords, res = 7, is_geojson = TRUE) {
-  UseMethod("polyfill", coords)
-}
-
-#' @name polyfill
-#' @export
-polyfill.matrix <- function(coords, res = 7, is_geojson = TRUE) {
-  h3js("polyfill", coords, res, is_geojson)
-}
-
-#' @name polyfill
-#' @export
-polyfill.data.frame <- function(coords, res = 7, is_geojson = TRUE) {
-  h3js("polyfill", as.matrix(coords), res, is_geojson)
-}
-
-#' @name polyfill
-#' @export
-polyfill.list <- function(coords, res = 7, is_geojson = TRUE) {
-  h3js_map2("polyfill", coords, res, is_geojson)
-}
-
-#' @name polyfill
-#' @export
-polyfill.sf <- function(coords, res = 7, is_geojson = TRUE) {
-  coords <- lapply(1:nrow(coords), function(i) sf::st_coordinates(coords[i, ])[, c("X", "Y")])
-  if (length(coords) == 1) return(h3js("polyfill", coords, res, is_geojson))
-
-  h3js_map2("polyfill", coords, res, is_geojson)
-}
-
 #' Get the outlines of a set of hexagons
 #'
 #' Returns GeoJSON MultiPolygon format
