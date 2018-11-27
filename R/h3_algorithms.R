55b022e847d8a2d3cf34f9a26c72bbae1397268c
skuethe <crazycapivara@gmail.com>
2018-11-27 22:02:53 +0100

    Refactor

--- R/h3_algorithms.R
+++ R/h3_algorithms.R
@@ -1,18 +1,14 @@
 #' Get the outlines of a set of hexagons
 #'
-#' Returns GeoJSON MultiPolygon format
-#' (an array of polygons, each with an array of loops, each an array of coordinates).
-#' Coordinates are returned as [lat, lng] pairs unless GeoJSON is requested.
-#'
 #' @inheritParams h3_to_geo_boundary
 #'
-#' @example inst/examples/api-reference/h3-set-to-multipolygon.R
+#' @example inst/examples/api-reference/h3-set-to-multi-polygon.R
 #'
 #' @export
 h3_set_to_multi_polygon <- function(h3_index, format_as_geojson = TRUE) {
-  res <- h3js("h3SetToMultiPolygon", h3_index, format_as_geojson) %>%
+  result <- h3js("h3SetToMultiPolygon", h3_index, format_as_geojson) %>%
     as.vector() %>% matrix(ncol = 2)
-  if (format_as_geojson) res %<>% h3forr_class("lng_lat_closed")
+  if (format_as_geojson) result %<>% h3forr_class("lng_lat_closed")
 
-  res
+  result
 }
--- inst/examples/api-reference/h3-set-to-multi-polygon.R
+++ inst/examples/api-reference/h3-set-to-multi-polygon.R
@@ -1,4 +1,5 @@
 coords <- c(37.3615593, -122.0553238)
+
 geo_to_h3(coords) %>%
   k_ring(3) %>%
   h3_set_to_multi_polygon() %>%
