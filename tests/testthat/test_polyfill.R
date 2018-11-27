3f6ff60a947bdb4be8f4cfb3aab1f8b8190b992a
skuethe <crazycapivara@gmail.com>
2018-11-27 21:21:08 +0100

    Add tests for polyfill with sf objects as input

--- tests/testthat/test_polyfill.R
+++ tests/testthat/test_polyfill.R
@@ -14,3 +14,20 @@ test_that("matrix and data frame", {
   expect_is(h3_indexes, "character")
   expect_equal(h3_indexes, h3_indexes2)
 })
+
+test_that("sf object", {
+  # Prepare
+  nc <- system.file("shape/nc.shp", package = "sf") %>%
+    sf::st_read(quiet = TRUE)
+
+  # Act
+  h3_index_list <- polyfill(nc[1:2, ], 5)
+  h3_indexes <- unlist(h3_index_list)
+  h3_indexes2 <- polyfill(nc[1, ], 5)
+
+  # Assert
+  expect_is(h3_index_list, "list")
+  expect_is(h3_indexes2, "character")
+  expect_length(h3_indexes, 6)
+  expect_equal(h3_index_list[[1]], h3_indexes2)
+})
