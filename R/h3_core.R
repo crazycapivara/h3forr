11f05faa6cd3c18cc9d671ad49eaf639c52d4da1
skuethe <crazycapivara@gmail.com>
2018-12-26 17:35:10 +0100

    Add some missing functions

--- R/h3_core.R
+++ R/h3_core.R
@@ -21,3 +21,43 @@ h3_is_valid <- function(h3_index) {
 h3_get_resolution <- function(h3_index) {
   h3js_map("h3GetResolution", h3_index)
 }
+
+#' Check whether the given H3 index is a pentagon
+#'
+#' @inheritParams h3_is_valid
+#'
+#' @return boolean vector
+#'
+#' @export
+### TODO: add test(s)
+h3_is_pentagon <- function(h3_index) {
+  h3js_map("h3IsPentagon", h3_index)
+}
+
+#' Check whether the given H3 index is in a Class III resolution
+#'
+#' Check whether the given H3 index is in a Class III resolution
+#' (rotated versus the icosahedron and subject to shape distortion adding extra points
+#' on icosahedron edges, making them not true hexagons).
+#'
+#' @inheritParams h3_is_valid
+#'
+#' @return boolean vector
+#'
+#' @export
+### TODO: add test(s)
+h3_is_res_class_iii <- function(h3_index) {
+  h3js_map("h3IsResClassIII", h3_index)
+}
+
+#' Get the number of the base cell for a given H3 index
+#'
+#' @inheritParams h3_is_valid
+#'
+#' @return numeric vector representing indexes of the base cells (1 - 122)
+#'
+#' @export
+### TODO: add test(s)
+h3_get_base_cell <- function(h3_index) {
+  h3js_map("h3GetBaseCell", h3_index) + 1
+}
