0ccac96ad9c7257b2ea87ba797495833bfff2b00
skuethe <crazycapivara@gmail.com>
2018-12-28 16:05:14 +0100

    Refactor hexbins using S3 methods

--- R/custom_utils.R
+++ R/custom_utils.R
@@ -27,7 +27,7 @@ get_sample_h3_edge_index <- function() {
   get_h3_unidirectional_edge(indexes[1], indexes[2])
 }
 
-count_h3 <- function(h3_index, to_sf = TRUE) {
+count_h3_obs <- function(h3_index, to_sf = TRUE) {
   freq_tbl <- table(h3_index) %>%
     # as.data.frame(responseName = "count") %>%
     tibble::as_tibble()
@@ -49,6 +49,57 @@ list_to_df <- function(x, key = "idx", value = "value") {
   df
 }
 
+# ---------
+count_h3 <- function(h3_indexes) {
+  column_names <- c("h3_index", "count")
+
+  h3_index_tbl <- table(h3_indexes) %>%
+    tibble::as_tibble()
+  colnames(h3_index_tbl) <- column_names
+
+  hexagons <- h3_to_geo_boundary(h3_index_tbl$h3_index) %>%
+    geo_boundary_to_sf()
+  hexagons[, column_names] <- h3_index_tbl
+
+  hexagons
+}
+
+#' Get hexbins for given points and resolution
+#'
+#' @inheritParams geo_to_h3
+#' @param data matrix, data frame (with latidude and longitude columns)
+#' or object of class \code{sf} (\code{POINT} geometry)
+#' @param ... ignored
+#'
+#' @name hexbins
+#' @export
+hexbins <- function(data, res = 7, ...) {
+  UseMethod("hexbins")
+}
+
+#' @name hexbins
+#' @export
+hexbins.sf <- function(data, res = 7, ...) {
+  h3_indexes <- geo_to_h3(data, res)
+  data$h3_index <- h3_indexes
+
+  hexagons <- count_h3(h3_indexes)
+
+  list(
+    hexagons = hexagons,
+    data = data
+  )
+}
+
+#' @param lat name of latitude column
+#' @param lng name of longitude column
+#'
+#' @name hexbins
+#' @export
+hexbins.data.frame <- function(data, res = 7, ..., lat = "lat", lng = "lng") {
+  sf::st_as_sf(data, coords = c(lng, lat), crs = 4326) %>%
+    hexbins(res)
+}
 #' Get hexbins
 #'
 #' @inheritParams geo_to_h3
@@ -57,7 +108,7 @@ list_to_df <- function(x, key = "idx", value = "value") {
 #' @param lng name of longitude column if matrix or data frame is supplied
 #'
 #' @export
-hexbins <- function(data, res = 7, lat = "lat", lng = "lng") {
+hexbins_legacy <- function(data, res = 7, lat = "lat", lng = "lng") {
   if (inherits(data, "matrix")) data %<>% as.data.frame()
 
   if (inherits(data, "data.frame")) {
