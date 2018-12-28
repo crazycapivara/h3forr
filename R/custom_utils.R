729583dcc2f7d6e8c2ad39bd045c01e213e4c048
skuethe <crazycapivara@gmail.com>
2018-12-28 14:42:04 +0100

    Add function 'hexbins'

--- R/custom_utils.R
+++ R/custom_utils.R
@@ -48,3 +48,36 @@ list_to_df <- function(x, key = "idx", value = "value") {
   names(df) <- c(key, value)
   df
 }
+
+#' Get hexbins
+#'
+#' @inheritParams geo_to_h3
+#' @param data matrix, data frame or \code{sf} object
+#' @param lat name of latitude column if matrix or data frame is supplied
+#' @param lng name of longitude column if matrix or data frame is supplied
+#'
+#' @export
+hexbins <- function(data, res = 7, lat = "lat", lng = "lng") {
+  if (inherits(data, "matrix")) data %<>% as.data.frame()
+
+  if (inherits(data, "data.frame")) {
+    data %<>% sf::st_as_sf(coords = c(lng, lat), crs = 4326)
+  }
+
+  h3_indexes <- geo_to_h3(data, res)
+  data$h3_index <- h3_indexes
+
+  h3_index_tbl <- table(h3_indexes) %>%
+    tibble::as_tibble()
+  colnames(h3_index_tbl) <- c("h3_index", "count")
+
+  hexagons <- h3_to_geo_boundary(h3_index_tbl$h3_index) %>%
+    geo_boundary_to_sf()
+
+  hexagons[, c("h3_index", "count")] <- h3_index_tbl
+
+  list(
+    hexagons = hexagons,
+    data = data
+  )
+}
