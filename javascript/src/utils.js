d6c27033bfc5d5203aa06c7e60e777be168b4059
skuethe <crazycapivara@gmail.com>
2018-12-31 18:15:39 +0100

    Add js function to count points

--- javascript/src/utils.js
+++ javascript/src/utils.js
@@ -1,5 +1,17 @@
 var utils = {};
 
+utils.countPoints = function(data, res) {
+  const layer = {};
+  data.forEach(function(item) {
+    const lat = item[0];
+    const lng = item[1];
+    const h3Index = h3.geoToH3(lat, lng, res);
+    layer[h3Index] = (layer[h3Index] || 0) + 1;
+  });
+
+  return parseLayer(layer);
+};
+
 utils.bufferPointsLinear = function(data, radius, res) {
   const h3Resolution = res || 8;
   const layer = {};
@@ -7,7 +19,7 @@ utils.bufferPointsLinear = function(data, radius, res) {
     const lat = item[0];
     const lng = item[1];
     const baseIndex = h3.geoToH3(lat, lng, h3Resolution);
-    // add multiple surrounding rings with less weight in each
+    // add rings with less weight in each
     const rings = h3.kRingDistances(baseIndex, radius);
     const step = 1 / (radius + 1);
     rings.forEach(function(ring, distance) {
@@ -17,7 +29,6 @@ utils.bufferPointsLinear = function(data, radius, res) {
     });
   });
 
-  // return layer;
   return parseLayer(layer);
 };
 
