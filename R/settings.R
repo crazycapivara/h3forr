1fc0663a078cd56aa3c73e657aa5edaa504dcbe3
skuethe <crazycapivara@gmail.com>
2018-12-31 18:13:52 +0100

    Add internal shortcut function to call js functions

--- R/settings.R
+++ R/settings.R
@@ -28,6 +28,10 @@ h3js_map2 <- function(func_name, data, ...) {
   pkg_env$ctx$call("vectorize2", func_name, data, list(...))
 }
 
+js <- function(...) {
+  pkg_env$ctx$call(...)
+}
+
 h3forr_class <- function(obj, class_name) {
   structure(obj, class = c(class(obj), class_name))
 }
