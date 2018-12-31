83c533ebafa8f67eb5d31941ae55dba57cebf681
skuethe <crazycapivara@gmail.com>
2018-12-31 18:17:25 +0100

    Add r-binding to count points

--- R/custom_buffer-points-linear.R
+++ R/custom_buffer-points-linear.R
@@ -1,19 +1,41 @@
 get_coords <- function(points) {
+  UseMethod("get_coords")
+}
+
+get_coords.matrix <- function(points) {
+  points
+}
+
+get_coords.data.frame <- function(points) {
+  as.matrix(points)
+}
+
+get_coords.sf <- function(points) {
   sf::st_coordinates(points)[, c("Y", "X")]
 }
 
 #' Buffer points linear
 #'
-#' @param points object of class \code{sf} with \code{POINT} geometry
+#' @param points matrix, data frame
+#' or object of class \code{sf} with \code{POINT} geometry
 #' @param radius number of rings (used as buffer around points)
-#' @param res resollution of hexagons to return
+#' @param res resolution of hexagons to return
 #'
 #' @example inst/examples/api-reference/buffer-points-linear.R
 #'
 #' @export
 buffer_points_linear <- function(points, radius = 2, res = 8) {
-  result <- pkg_env$ctx$call("utils.bufferPointsLinear", get_coords(points), radius, res) %>%
+  result <- js("utils.bufferPointsLinear", get_coords(points), radius, res) %>%
     tibble::as_tibble()
   result$norm <- scales::rescale(result$weight, c(0, 1))
   result
+}
+
+#' Count points in each hexagon for a given resolution
+#'
+#' @inheritParams buffer_points_linear
+#'
+#' @export
+count_points <- function(points, res = 7) {
+  js("utils.countPoints", get_coords(points), res)
 }
