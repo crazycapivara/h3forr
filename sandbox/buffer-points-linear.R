a9b0be6d2c6d0907c4a15786dad5531ab2f25978
skuethe <crazycapivara@gmail.com>
2019-01-02 19:45:22 +0100

    Add buffer example to sandbox

--- sandbox/buffer-points-linear.R
+++ sandbox/buffer-points-linear.R
@@ -1,20 +1,40 @@
 ### buffer points linear
 library(sf)
+library(purrr)
+library(data.table)
+library(scales)
 
 bart_stations <- system.file("geojson/bart_stations.geojson", package = "h3forr") %>%
   st_read(quiet = TRUE)
 
 radius <- 2
 
-### this should already be done in k_ring_distances
+hex <- geo_to_h3(bart_stations, res = 8) %>%
+  k_ring_distances(radius) %>%
+  reduce(rbind) %>%
+  as.data.table()
+
+step <- 1 / (radius + 1)
+hex$weight <- 1 - hex$distance * step
+hex <- hex[, .(weight = sum(weight)), .(h3_index)]
+hex$norm <- rescale(hex$weight, c(0, 1))
+
+### this should already be done in k_ring_distancesm (done! see above)
 x <- geo_to_h3(bart_stations, res = 8) %>%
   k_ring_distances(radius) %>%
   lapply(h3forr_unnest, name = "distance", value = "h3_index") %>%
-  do.call(rbind, .)
+  do.call(rbind, .) %>% as.data.table()
 x$distance <- x$distance - 1
 
 step <- 1 / (radius + 1)
 
+x$weight <- 1 - x$distance * step
+x <- x[, .(weight = sum(weight)), .(h3_index)]
+# x[, weight := sum(weight), .(h3_index)]
+x[, norm := scales::rescale(weight, c(0, 1))]
+
+
+### old approach
 layer <- list()
 
 for (i in 1:nrow(x)) {
