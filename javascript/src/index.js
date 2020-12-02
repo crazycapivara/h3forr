9e70077c902364787084d129a8e5783e443eab24
skuethe <crazycapivara@gmail.com>
2020-12-02 18:51:35 +0100

    Upgrade to h3-js v3.7.0

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,4 +1,5 @@
 require("@babel/polyfill");
+global.document = { }; // Fix, because 'document' needs to be defined
 global.h3 = require("h3-js");
 global.vectorize = require("./vectorize.js");
 global.vectorize2 = require("./vectorize2.js");
